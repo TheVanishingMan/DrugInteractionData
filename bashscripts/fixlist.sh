0991c2be82516401aeaa2eaac3f1f867203d1d00
TheVanishingMan <alexander@batflyer.net>
2016-06-14 12:31:09 -0400

    Ran fixlist.sh and rxdownloader.sh to find missing drugs, and sure enough I found 756 of them.  This still falls 18 short of the total I came up with, will push in the future when I find the issue.

--- bashscripts/fixlist.sh
+++ bashscripts/fixlist.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+INPUTFILE=drugslist.txt
 OUTPUTFILE=checkup.txt
+START=0
 
 function checkfile {
-    while [ $(wc --bytes drugslist.txt | cut -d ' ' -f 1) -gt 0 ]; do
-	DRUGNAME=$(head -n 1 drugslist.txt)
+    while [ $(wc --bytes $INPUTFILE | cut -d ' ' -f 1) -gt 0 ]; do
+	DRUGNAME=$(head -n 1 $INPUTFILE)
 	TEST=`grep $DRUGNAME' ' drugInteractionsFolder/LOG.txt`
+	echo Progress: $START " / " $TOTAL
 	if [[ -z $TEST ]]; then
-	    echo "$DRUGNAME1" >> $OUTPUTFILE
+	    echo $TEST
+	    echo "$DRUGNAME" >> $OUTPUTFILE
 	fi
-	tail -n +2 drugslist.txt > drugslist.tmp && mv drugslist.tmp drugslist.txt
+	tail -n +2 $INPUTFILE > drugslist.tmp && mv drugslist.tmp $INPUTFILE
+	let START+=1
     done
 }
 
-touch $OUTPUTFILE
-checkfile
+#MAIN
+rm -f $INPUTFILE          # Delete whatever exists in drugslist.txt
+bash builddruglist.sh     # Download a fresh copy of drugslist.txt
+TOTAL=$(wc --lines $INPUTFILE | cut -d ' ' -f 1) # Set total (for printing)
+touch $OUTPUTFILE         # Create checkup.txt to pass information into
+checkfile                 # Check LOG.txt for missing drugs
+mv $OUTPUTFILE $INPUTFILE # Update drugslist.txt
