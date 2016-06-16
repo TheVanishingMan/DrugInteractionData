cf276e0deda6c4ceb43e066cf69d9a8dae54ca42
TheVanishingMan <alexander@batflyer.net>
2016-06-16 14:10:09 -0400

    Fixed file extension on columnsplitting.rkt, added quite a bit to pullabstracts.sh.  Now runs through the lists in sequence (n^2 / 2).

--- Generated/pullabstracts.sh
+++ Generated/pullabstracts.sh
@@ -11,30 +11,43 @@ function shrink2 {
     tail -n +2 "$FILE2" > drugs2.tmp && mv drugs2.tmp "$FILE2"
 }
 
+function newline {
+    echo " "
+}
+
+#commands for testing
+rm -f LOG.txt
+cp STABLE.txt drugs1.txt
+cp STABLE.txt drugs2.txt
+rm -f Abstracts/*
+#
+
 FILE1=drugs1.txt
 FILE2=drugs2.txt
 STABLE=STABLE.txt
 BEGIN=`wc --lines drugs1.txt | cut -d ' ' -f 1`
 
 while [ $(wc --bytes $FILE1 | cut -d ' ' -f 1) -gt 0 ]; do
-    while [ $(wc --bytes $FILE2 | cut -d ' ' -f 1) -gt 0 ]; do
+    DRUG1=`head -n 1 "$FILE1"`
+    DRUG2=`head -n 1 "$FILE2"`
+    #if [ $DRUG1 = $DRUG2 ]; then
+#	shrink1
+ #   fi
+    until [ $DRUG1 = $DRUG2 ]; do
 	DRUG1=`head -n 1 "$FILE1"`
 	DRUG2=`head -n 1 "$FILE2"`
-	if [ $DRUG1 = $DRUG2 ]; then
-	    tail -n +2 "$FILE2" > drugs2.tmp && mv drugs2.tmp "$FILE2"
-	    DRUG2=`head -n 1 "$FILE2"`
-	fi
 	TEST=`perl pmsearch -c -t 800 -d 50 $DRUG1 $DRUG2`
-	echo " "
-	echo Found $TEST for $DRUG1 and $DRUG2 " | " Progress: $[`wc --lines drugs1.txt | cut -d ' ' -f 1`-$BEGIN] / $BEGIN
+	echo Found $TEST for $DRUG1 and $DRUG2 " | " Progress: $[$BEGIN-`wc --lines drugs1.txt | cut -d ' ' -f 1`] / $BEGIN
 	if [ $TEST -gt 0 ]; then
-	    FILENAME=$DRUG1-$DRUG2.txt
-	    echo Creating $FILENAME
-	    perl pmsearch -t 800 -d 50 $DRUG1 $DRUG2 | perl pmid2text -a -i > Abstracts/$FILENAME
-	    echo " "
-	    shrink2
+	    if [ $DRUG1 = $DRUG2 ]; then
+		shrink2
+	    else
+		FILENAME=$DRUG1-$DRUG2.txt
+		echo Creating $FILENAME && echo Creating $FILENAME, found $TEST results. >> LOG.txt
+		perl pmsearch -t 800 -d 50 $DRUG1 $DRUG2 | perl pmid2text -a -i > Abstracts/$FILENAME
+		shrink2
+	    fi
 	else
-	    echo " "
 	    shrink2
 	fi
     done
