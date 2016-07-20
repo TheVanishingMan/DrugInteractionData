63344989db9045f96acf745df60776548e520d26
TheVanishingMan <alexander@batflyer.net>
2016-07-20 16:51:21 -0400

    Removed files from branch that I moved into another folder.

--- bashscripts/replaceSpecialChars.sh
+++
@@ -1,10 +0,0 @@
-#!/bin/bash
-FILE=$1
-echo $FILE
-cat charactersToReplace.txt | 
-	while read -r line; do 
-		char=$(echo $line | cut -d',' -f1)
-		replace=$(echo $line | cut -d',' -f2)
-		sed -i "s/$char/$replace/Ig" $FILE
-	done
-sed -i 's///g' $FILE
