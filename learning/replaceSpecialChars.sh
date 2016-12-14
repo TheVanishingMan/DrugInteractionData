cb176958b54e9e97828604d67a65da277e0a1b76
TheVanishingMan <alexander@batflyer.net>
2016-12-14 17:31:25 -0500

    I'm pushing everything up to github before I push this into my backup storage.

--- learning/replaceSpecialChars.sh
+++ learning/replaceSpecialChars.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-FILE=$1
-echo $FILE
-sed -n 'l0' $1
-cat charactersToReplace.txt | 
-	while read -r line; do 
-		char=$(echo $line | cut -d',' -f1)
-		replace=$(echo $line | cut -d',' -f2)
-		sed -i "s/$char/$replace/Ig" $FILE
-	done
-sed -i 's///g' $FILE
+NAME=$1
+FILE=`cat $1`
+FILE2=TEMPORARY.tmp
+
+SIMPLIFIED=`echo "$FILE" | sed -n 'l0' | grep -v "\-\-"`
+DRUGINTERACTIONS=`echo "$SIMPLIFIED" | grep -A 1 "\"drug_interactions\"" | grep -v "drug_interactions" | grep -v "\-\-" | sed -r 's/^[[:blank:]]+//'`
+ADVERSEREACTIONS=`echo "$SIMPLIFIED" | grep -A 1 "\"adverse_reactions\"" | grep -v "adverse_reactions" | grep -v "\-\-" | sed -r 's/^[[:blank:]]+//'`
+WARNINGS=`echo "$SIMPLIFIED" | grep -A 1 "\"warnings_and_cautions\"" | grep -v "warnings_and_cautions" | grep -v "\-\-" | sed -r 's/^[[:blank:]]+//'`
+echo "$DRUGINTERACTIONS" > $FILE2
+echo "$ADVERSEREACTIONS" >> $FILE2
+echo "$WARNINGS" >> $FILE2
+
+sort -u $FILE2 > ABCDE.tmp && mv ABCDE.tmp $FILE2
+mv $FILE2 $NAME
